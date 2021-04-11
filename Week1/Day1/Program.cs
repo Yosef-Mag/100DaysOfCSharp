5d3f7ddb358ac2d14116e007e727d8c37df189ea
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-11 21:28:13 +0300

    Week1 day1 odeOrEven

---
+++ Week1/Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int number;
+            Console.Write("Enter the number :");
+                number = Convert.ToInt16(Console.ReadLine());
+
+                if ( number % 2 == 0)
+                    Console.Write("Even");
+                else 
+                    Console.Write("Odd");
+
+            Console.ReadKey();
+        }
+    }
+}
