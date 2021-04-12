6f46b78d2f9f47d95bbde328e6646c21fd98877b
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-12 21:16:15 +0300

    Week1 day2 Simple calculator

---
+++ Week1/Day2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            newOne :
+
+            Console.WriteLine("Enter number 1 : ");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter number 2 : ");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            int result;
+
+            Console.WriteLine("Choose operation (+,-,*,/) : ");
+            string operation = Console.ReadLine();
+          
+            switch(operation)
+            {
+                case "+" :
+                    result = num1 + num2;
+                    Console.WriteLine("Number1 + Number2 = " + result);
+                    break;
+                case "-" :
+                    result = num1 - num2;
+                    Console.WriteLine("Number1 - Number2 = " + result);
+                    break;
+                case "*" :
+                    result = num1 * num2;
+                    Console.WriteLine("Number1 * Number2 = " + result);
+                    break;
+                case "/" :
+                    result = num1 / num2;
+                    Console.WriteLine("Number1 / Number2 = " + result);
+                    break;
+                default:
+                    Console.Write("Input error");
+                    break;
+            }
+
+            goto newOne;
+
+            Console.ReadKey();
+        }
+    }
+}
