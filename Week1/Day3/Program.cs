68df009b2e22865f3776928a804f403a5ed00a6d
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-13 23:18:29 +0300

    Week1 day3 alternateCase

---
+++ Week1/Day3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace Day3
+{
+    class Program
+    {
+        public static string alternateCase(string s)
+        {
+            return string.Concat(s.Select(e => char.IsUpper(e) ? char.ToLower(e) : char.ToUpper(e)));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(alternateCase("Hello World"));
+
+            Console.ReadKey();
+        }
+    }
+}
