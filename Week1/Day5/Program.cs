884b6eb1067a39b1b960ae0437315f8dbad77cae
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-15 22:17:52 +0300

    Week1 day5 FindShort

---
+++ Week1/Day5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace Day5
+{
+    class Program
+    {
+        public static int FindShort(string s)
+        {
+            return s.Split(' ').Min(x => x.Length);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindShort("Yosef Moammed is learning C#"));
+
+            Console.ReadKey();
+        }
+    }
+}
