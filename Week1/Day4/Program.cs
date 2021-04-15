7fb5e33aee97eb0816a767c5175401f8c46aaa91
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-15 19:00:00 +0300

    Week1 day4 geetMiddle

---
+++ Week1/Day4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    { 
+        public static string GetMiddle(string s)
+        {
+           return string.IsNullOrEmpty(s)
+            ? s
+            : s.Substring((s.Length - 1) / 2, 2 - s.Length % 2);     
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMiddle("test"));
+
+             Console.ReadKey();
+        }
+    }
+}
