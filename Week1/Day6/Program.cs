bc7acf2dac4ec761a2e685991d5ba0f047a478e7
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-16 19:00:00 +0300

    Week1 day6 Accum

---
+++ Week1/Day6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Day6
+{
+    class Program
+    {
+    public static String Accum(string s) 
+    {
+        return string.Join("-",s.Select((x,i)=>char.ToUpper(x)+new string(char.ToLower(x),i)));
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accum("RqaEzty"));
+
+            Console.ReadKey();
+        }
+    }
+}
