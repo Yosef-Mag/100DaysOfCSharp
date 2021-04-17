67f7cc9863c3687983f741e3b52c081eca8332f9
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-17 17:51:11 +0300

    Week1 day6 using Vowelremover

---
+++ Week1/Day7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Day7
+{
+    class Program
+    {
+    public static string Vowelremover(string input)
+        {
+            return Regex.Replace(input, "[aeiou]", "");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Vowelremover("goodbye"));
+
+            Console.ReadKey();
+        }
+    }
+}
