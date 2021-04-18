4892bcd0fd76bc3bb604cc05a507728d7a747495
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-18 21:48:14 +0300

    Week2 day1 HexToDec

---
+++ Week2/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        public static int HexToDec(string hexString)
+        {
+            return Convert.ToInt32(hexString.TrimStart('-'), 16) * (hexString[0] == '-' ? -1 : 1);
+        }
+        static void Main(string[] args)
+        {
+             Console.WriteLine(HexToDec("a"));
+
+             Console.ReadKey();
+        }
+    }
+}
