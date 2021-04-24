1df40e446de3cdbaf4ed56b4e25bf85d885eead7
Yosef-Mag <yosef.maghaireh@gmail.com>
2021-04-24 15:23:12 +0300

    Week2 day6 CreatePhoneNumber

---
+++ Week2/Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            return long.Parse(string.Concat(numbers)).ToString("(000) 000-0000");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+            Console.ReadKey();
+        }
+    }
+}
